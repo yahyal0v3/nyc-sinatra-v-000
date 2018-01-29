2c2fcbfff619a4a3382520759944c70580a30d0f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-01-29 21:18:22 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,39 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks' do
+    erb :'/landmarks/index'
+  end
+
+  post '/landmarks' do
+    landmark = Landmark.create(params[:landmark])
+
+    if !params[:figure][:name].empty?
+      landmark.figure = Figure.create(params[:figure])
+    end
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.update(params[:landmark])
+
+    if !params[:figure][:name].empty?
+      landmark.figure = Figure.create(params[:figure])
+    end
+    redirect "/landmarks/#{landmark.id}"
+  end
 end
